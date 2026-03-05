6d81b833fb9d0ef00b4ee6bf4968dc0e47b440bf
francivaldo4334 <francivaldodev@gmail.com>
2026-03-05 16:51:05 -0300

    save

--- product/admin.py
+++ product/admin.py
@@ -3,5 +3,9 @@ from product.models import *
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Product)
 admin.site.register(PriceHistory)
+
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        return ('code',) if obj else ()
