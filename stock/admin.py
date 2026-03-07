b3f747daada8535f53015f0f8097d4115d062709
francivaldo4334 <francivaldodev@gmail.com>
2026-03-07 13:31:34 -0300

    save

--- stock/admin.py
+++ stock/admin.py
@@ -16,3 +16,11 @@ class StockMovimentFixed(admin.ModelAdmin):
     exclude = ['origin', 'destination']
     def get_readonly_fields(self, request, obj=None):
         return ('product', 'quantity', 'reason', 'origin', 'destination') if obj else ()
+admin.site.register(Category)
+admin.site.register(PriceHistory)
+admin.site.register(UnitType)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        return ('code',) if obj else ()
--- system/settings.py
+++ system/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'schedule',
     'sale',
-    'product',
     'stock',
 ]
 
