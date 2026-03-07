6656ccae592d48afb8e1a96c72ad57fe5dd9115e
francivaldo4334 <francivaldodev@gmail.com>
2026-03-07 14:12:09 -0300

    save

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,30 @@
+# pyright: reportIncompatibleVariableOverride=false
 from django.db import models
+from core.models import ActivatorModel, CreatedByModel, TimeStampedModel
+from django.conf import settings
+from dateutil.rrule import rrulestr
 
 # Create your models here.
+def is_valid_rrule(value):
+    try:
+        rrulestr(value)
+        return True,
+    except Exception:
+        return False
+
+class Schedule(TimeStampedModel, ActivatorModel):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE)
+    rrule_params = models.CharField(validators=[is_valid_rrule])
+    duration = models.DurationField()
+    init = models.DateTimeField()
+    end = models.DateTimeField()
+
+
+class Appointment(
+    TimeStampedModel,
+    ActivatorModel,
+    CreatedByModel,
+):
+    schedule = models.ForeignKey(Schedule, models.CASCADE)
+    init = models.DateTimeField()
+    end = models.DateTimeField()
