bd7d886d6ca3515953a70c1e5ea14da5c501e10d
francivaldo4334 <francivaldodev@gmail.com>
2026-03-08 16:01:52 -0300

    save

--- core/urls.py
+++ core/urls.py
@@ -16,13 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
+def to_app_page(request):
+    return redirect('/app/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('', include('common.urls')),
+    path('', to_app_page),
+    path('app/', include('app.urls')),
     path('', include('stock.urls')),
 ]
--- static/custom-tags.js
+++ static/custom-tags.js
@@ -2,3 +2,16 @@
 // footer
 // section
 // main
+class AppLayout extends HTMLDivElement {
+  constructor() {
+    super();
+    this.style.display = 'grid'
+    this.style.gridTemplateColumns = 'minmax(0, 15rem) 1fr'
+    this.style.width = '100dvw'
+    this.style.height = '100dvh'
+    this.style.margin = '0'
+    this.style.overflow = 'hidden'
+  }
+}
+
+window.customElements.define('app-layout', AppLayout, { extends: 'div' })
