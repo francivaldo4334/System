4c35c573dd57958afd8031365ddc140429c08c73
francivaldo4334 <francivaldodev@gmail.com>
2026-03-10 11:25:20 -0300

    sv

--- static/state.js
+++ static/state.js
@@ -1,4 +1,5 @@
 const states = new Map();
+const stateAt = new Map();
 
 const getState = (key) => states.get(key)
 const setState = (key, newValue) => {
@@ -13,16 +14,23 @@ const setState = (key, newValue) => {
   }
 }
 class StateDef extends HTMLElement {
+  constructor(){
+    super();
+    const name = this.getAttribute('name')
+    const at = this.getAttribute("at")
+    if (at) stateAt.set(name, at)
+  }
   connectedCallback() {
     this.name = this.getAttribute('name')
     setState(this.name, this.getAttribute('value'))
   }
   disconnectedCallback() {
     states.delete(this.name)
+    stateAt.delete(this.name)
   }
   static observedAttributes = ['value']
-  attributeChangedCallback(name, oldValue, newValue){
-    if (oldValue != newValue){
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue != newValue) {
       const name = this.getAttribute('name')
       setState(name, newValue)
     }
@@ -32,10 +40,10 @@ window.addEventListener('state-update', (event) => {
   const { key, value } = event.detail
   if (!key) return;
   document.querySelectorAll(`[data-state="${key}"]`).forEach(el => {
-    const target = el.dataset.at || 'textContent'
+    const target = el.dataset.at || stateAt.get(key) || 'textContent'
     if (target in el && el[target] !== value) {
       el[target] = value;
-    } else if(el.getAttribute(target) !== String(value)) {
+    } else if (el.getAttribute(target) !== String(value)) {
       el.setAttribute(target, value);
     }
   })
