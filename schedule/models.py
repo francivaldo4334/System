6911e6ec71afb47adcb2767f9a592c454c196a0a
francivaldo4334 <francivaldodev@gmail.com>
2026-03-16 16:40:31 -0300

    save

--- schedule/models.py
+++ schedule/models.py
@@ -12,19 +12,18 @@ def is_valid_rrule(value):
     except Exception:
         return False
 
-class Schedule(TimeStampedModel, ActivatorModel):
+class Availability(TimeStampedModel, ActivatorModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE)
     rrule_params = models.CharField(validators=[is_valid_rrule])
     duration = models.DurationField()
-    init = models.DateTimeField()
-    end = models.DateTimeField()
+    start_at = models.DateTimeField()
+    finish_at = models.DateTimeField()
 
 
 class Appointment(
     TimeStampedModel,
-    ActivatorModel,
     CreatedByModel,
 ):
-    schedule = models.ForeignKey(Schedule, models.CASCADE)
-    init = models.DateTimeField()
-    end = models.DateTimeField()
+    availability = models.ForeignKey(Availability, models.CASCADE)
+    start_at = models.DateTimeField()
+    duration = models.DurationField()
