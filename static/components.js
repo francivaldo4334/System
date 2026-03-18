f38f266d7f9d7a97b098382a8f2b01dc98bf9e57
francivaldo4334 <francivaldodev@gmail.com>
2026-03-18 20:00:53 -0300

    sve

--- static/components.js
+++ static/components.js
@@ -122,6 +122,56 @@ class EanCodeField extends HTMLInputElement {
     return actualDigit === expectedDigit;
   }
 }
+class CustomAjaxForm extends HTMLFormElement {
+  constructor() {
+    super();
+    this._onSubmit = this._onSubmit.bind(this);
+  }
+
+  connectedCallback() {
+    const store = this.getAttribute('store');
+    if (!store) throw TypeError("'store' is required.");
+
+    this.store = Object.freeze({
+      data: `${store}.data`,
+      isLoading: `${store}.isLoading`,
+      status: `${store}.status`,
+      error: `${store}.error`,
+    });
+
+    setState(this.store.data, '{}');
+    setState(this.store.isLoading, 'false');
+    setState(this.store.error, '');
+    setState(this.store.status, '');
+
+    this.endpoint = this.getAttribute('endpoint');
+    this.method = (this.getAttribute('method') || 'GET').toUpperCase();
+
+    this.addEventListener('submit', this._onSubmit);
+  }
+
+  disconnectedCallback() {
+    Object.values(this.store).forEach(k => typeof states !== 'undefined' && states.delete(k));
+    this.removeEventListener('submit', this._onSubmit);
+  }
+  _onSubmit(event) {
+    event.preventDefault();
+    setState(this.store.isLoading, 'true')
+    fetch(this.endpoint, { method: this.method })
+      .then(response => {
+        const status = response.status;
+        setState(this.store.status, status)
+        if (response.ok) {
+          response.text().then(data => setState(this.store.data, data))
+          return
+        }
+        response.text().then(data => setState(this.store.error, data))
+      })
+      .finally(() => setState(this.store.isLoading, 'false'))
+  }
+}
+
 window.customElements.define('app-field', FieldControl)
 window.customElements.define('app-input-currency', CurrencyField, { extends: 'input' })
 window.customElements.define('app-input-ean', EanCodeField, { extends: 'input' })
+window.customElements.define('app-ajax-form', CustomAjaxForm, { extends: 'form' })
