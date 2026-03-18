4c785b8e74a9f68d9e24beffb31995336219f860
francivaldo4334 <francivaldodev@gmail.com>
2026-03-18 19:21:24 -0300

    save

--- static/components.js
+++ static/components.js
@@ -1,28 +1,127 @@
-class AppNavItem extends HTMLElement {
+// class AppNavItem extends HTMLElement {
+//   connectedCallback() {
+//     const store = this.getAttribute('store')
+//     const endpoint = this.getAttribute('endpoint')
+//     const content = this.innerHTML;
+//     const active = this.hasAttribute('default')
+//     this.innerHTML = `
+//       <form is="app-ajax-form"
+//             store="${store}"
+//             endpoint="${endpoint}">
+//           <button class="btn btn-ghost w-full text-start flex items-center gap-x ${active ? 'active' : ''} "
+//             type = "submit"
+//             style = "--gx:var(--s2)" > ${content}</button >
+//       </form >
+//   `
+//     this.querySelector('button').addEventListener('click', (e) => {
+//       if (e.target.classList.contains('active'))
+//         return e.preventDefault();
+//       const btns = document.querySelectorAll('form[is="app-ajax-form"] button')
+//       btns.forEach(btn => {
+//         btn.classList.remove('active')
+//       })
+//       e.target.classList.add('active')
+//     })
+//   }
+// }
+
+class FieldControl extends HTMLElement {
+  static observedAttributes = ['invalid', 'success', 'error', 'label', 'help'];
+  constructor() {
+    super();
+    this._initialContent = this.innerHTML;
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (this.isConnected && oldValue !== newValue) {
+      this.render();
+    }
+  }
+
   connectedCallback() {
-    const store = this.getAttribute('store')
-    const endpoint = this.getAttribute('endpoint')
-    const content = this.innerHTML;
-    const active = this.hasAttribute('default')
+    if (!this._initialContent) this._initialContent = this.innerHTML;
+    this.render();
+  }
+
+  render() {
+    const label = this.getAttribute('label') || '';
+    const error = this.getAttribute('error') || '';
+    const help = this.getAttribute('help') || '';
+    const isInvalid = this.hasAttribute('invalid');
+    const isSuccess = this.hasAttribute('success');
+    const id = this.getAttribute('for');
     this.innerHTML = `
-      <form is="app-ajax-form"
-            store="${store}"
-            endpoint="${endpoint}">
-          <button class="btn btn-ghost w-full text-start flex items-center gap-x ${active ? 'active' : ''} "
-            type = "submit"
-            style = "--gx:var(--s2)" > ${content}</button >
-      </form >
-  `
-    this.querySelector('button').addEventListener('click', (e) => {
-      if (e.target.classList.contains('active'))
-        return e.preventDefault();
-      const btns = document.querySelectorAll('form[is="app-ajax-form"] button')
-      btns.forEach(btn => {
-        btn.classList.remove('active')
-      })
-      e.target.classList.add('active')
-    })
+      <div class="flex flex-col">
+          <label${id ? ' for="' + id + '"' : ''}><strong>${label}</strong></label>
+          ${this._initialContent}
+          <div class="border-t m-x" style="--mx:var(--b2);${isInvalid ? '--btc:var(--c-error)' : isSuccess ? '--btc:var(--c-success)' : 'display:none'}"></div>
+          <small role="alert"
+                 class="text-color"
+                 style="--tc:var(--c-error); --text-sm-lh:var(--s4);display: ${error && isInvalid ? 'block' : 'none'}">
+            ${error}
+          </small>
+          <small style="display: ${error ? 'block' : 'none'}">
+            ${help}
+          </small>
+      </div>
+    `;
   }
 }
+class CurrencyField extends HTMLInputElement {
+  get valueAsDecimal() {
+    const cleanValue = this.value.replace(/\D/g, '');
+    const digits = cleanValue.slice(0, -2).replace(/^0+(?=\d)/, '') || '0'
+    const decimals = cleanValue.slice(-2).padStart(2, '0')
+    return `${digits}.${decimals}`
+  }
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.inputMode = 'decimal';
+    this.maxLength = 29;
+    if (!this.value || this.value === "") this.formatCurrency();
+    this.addEventListener('input', () => this.formatCurrency())
+  }
+  formatCurrency() {
+    let floatValue = this.valueAsDecimal;
+    this.value = floatValue.toLocaleString('pt-BR', {
+      maximumFractionDigits: 2,
+      minimumFractionDigits: 2,
+    })
+    this.setSelectionRange(this.value.length, this.value.length)
+  }
+}
+class EanCodeField extends HTMLInputElement {
+  constructor() {
+    super();
+  }
+  checkValidity() {
+    debugger
+    const code = this.value;
+    const calcCheckDigit = (baseCode = "") => {
+      const sum = baseCode
+        .split('')
+        .reverse()
+        .reduce((acc, n, i) => {
+          const weight = i % 2 === 0 ? 3 : 1;
+          return acc + weight * parseInt(n, 10);
+        }, 0);
 
-customElements.define('app-nav-option', AppNavItem)
+      return ((10 - (sum % 10)) % 10).toString();
+    }
+
+    const cleanCode = code.replace(/\D/g, '')
+
+    if (![14, 13, 12, 8].includes(cleanCode.length))
+      return false;
+    const base = cleanCode.slice(0, -1);
+    const actualDigit = cleanCode.slice(-1);
+    const expectedDigit = calcCheckDigit(base);
+
+    return actualDigit === expectedDigit;
+  }
+}
+window.customElements.define('app-field', FieldControl)
+window.customElements.define('app-input-currency', CurrencyField, { extends: 'input' })
+window.customElements.define('app-input-ean', EanCodeField, { extends: 'input' })
