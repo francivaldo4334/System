acc279724ac8b738319946f0a73d43faade1e2b7
francivaldo4334 <francivaldodev@gmail.com>
2026-03-22 19:54:19 -0300

    save

--- app/views.py
+++ app/views.py
@@ -5,30 +5,23 @@ from django.utils import timezone
 
 # Create your views here.
 class AppView(TemplateView):
-    template_name="pages/app/index.html"
-    extra_context = {
-      'app_template_name_options': [
-          {
-              'url_name': 'app-schedule',
-              'title': _('Agenda'),
-              'icon_template_name': 'icons/calendar.svg'
-          }
-      ],
-      'default_app_template_name': 'pages/app/schedule/index.html#main',
-      'today': f'{timezone.localtime(timezone.now()).date().isoformat()}T00:00:00',
-    }
-    def get_template_names(self): # type: ignore
-        app_section = self.request.META.get('HTTP_AJAX_PARTIAL')
-        if app_section:
-            return [f"{self.template_name}#{app_section}"]
-        return [self.template_name]
+    template_name = "pages/app/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Dados da UI movidos para cá para serem dinâmicos
+        context.update({
+            'app_template_name_options': [
+                {
+                    'url_name': 'app-schedule',
+                    'title': _('Agenda'),
+                    'icon_template_name': 'icons/calendar.svg'
+                }
+            ],
+            'default_app_template_name': 'pages/app/schedule/index.html#main',
+            'today': f'{timezone.localtime(timezone.now()).date().isoformat()}T00:00:00',
+        })
+        return context
 
 class AppScheduleView(AppView):
     template_name = 'pages/app/schedule/index.html'
-    def get_context_data(self, **kwargs):
-        from schedule.models import Service, ResourceSelectable
-        kwargs.update({
-              'services': Service.objects.all(), # type: ignore
-              'resources': ResourceSelectable.objects.all(),
-          })
-        return super().get_context_data(**kwargs)
