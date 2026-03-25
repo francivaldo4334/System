a4f25dd96cdc6936b2bdc9d2afe5e1af87c85447
francivaldo4334 <francivaldodev@gmail.com>
2026-03-25 09:54:31 -0300

    save

---
+++ static/query-manager.js
@@ -0,0 +1,30 @@
+const queryCacheFactory = () => {
+  const _internalCache = new Map();
+  return async function useQueryCache(queryKey, fn, onSuccess, onError, ttl = 0) {
+    const now = Date.now();
+    try {
+      if (_internalCache.has(queryKey)) {
+        const { data, timestamp } = _internalCache.get(queryKey);
+        const isExpired = now - timestamp > ttl;
+        if (!isExpired) {
+          if (onSuccess) onSuccess(data);
+          return data;
+        }
+        _internalCache.delete(queryKey);
+      }
+      const data = await fn();
+      _internalCache.set(queryKey, {
+        data,
+        timestamp: now
+      });
+
+      if (onSuccess) onSuccess(data);
+      return data;
+    } catch (error) {
+      if (onError) onError(error);
+      throw error;
+    }
+  };
+};
+
+const $q = queryCacheFactory();
