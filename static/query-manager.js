11a1b04c59eb6004c35e6b32ec9ce6ea751e8cf2
francivaldo4334 <francivaldodev@gmail.com>
2026-03-25 10:30:00 -0300

    save

--- static/query-manager.js
+++ static/query-manager.js
@@ -1,30 +1,39 @@
 const queryCacheFactory = () => {
   const _internalCache = new Map();
-  return async function useQueryCache(queryKey, fn, onSuccess, onError, onFinally, ttl = 0) {
+  return async function useQueryCache(queryKey, url, onSuccess, onError, onFinally, ttl = 0,selection = (r) => r.json() ) {
     const now = Date.now();
     try {
       if (_internalCache.has(queryKey)) {
-        const { data, timestamp } = _internalCache.get(queryKey);
-        const isExpired = now - timestamp > ttl;
-        if (!isExpired) {
-          if (onSuccess) onSuccess(data);
-          return data;
+        const { data, status, timestamp } = _internalCache.get(queryKey);
+        if (now - timestamp <= ttl) {
+          onSuccess?.(data, status);
+          return response;
         }
         _internalCache.delete(queryKey);
       }
-      const data = await fn();
-      _internalCache.set(queryKey, {
-        data,
-        timestamp: now
-      });
+      const response = await fetch(url);
+      const isSuccess = response.status >= 200 && response.status < 300;
 
-      if (onSuccess) onSuccess(data);
-      return data;
+      if (isSuccess) {
+        let data;
+          data = await selection(response)
+        _internalCache.set(queryKey, { data, status: response.status, timestamp: now });
+        onSuccess?.(data, response.status);
+      } else {
+        const errorContext = { 
+          status: response.status,
+          data: response.data 
+        };
+        throw errorContext;
+      }
+      return response;
     } catch (error) {
-      if (onError) onError(error);
+      if (onError) {
+        onError(error.status ? error : { message: error.message, status: 500 });
+      }
       throw error;
     } finally {
-      onFinally?.()
+      onFinally?.();
     }
   };
 };
