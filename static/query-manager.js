48a0a0bf9d6a6f19833898e5e4e7bb47637857c1
francivaldo4334 <francivaldodev@gmail.com>
2026-03-25 10:52:05 -0300

    save

--- static/query-manager.js
+++ static/query-manager.js
@@ -1,40 +1,47 @@
 const queryCacheFactory = () => {
   const _internalCache = new Map();
-  return async function useQueryCache(queryKey, url, onSuccess, onError, onFinally, ttl = 0,selection = (r) => r.json() ) {
+  const _watchedQueries = new Map();
+  const refetch = async (queryKey) => {
+      const config = _watchedQueries.get(queryKey)
+      await useQueryCache(queryKey, config.url, config.callbacks, config.options);
+  }
+  window.addEventListener('focus', () => {
+    _watchedQueries.forEach(async (config, queryKey) => {
+      await useQueryCache(queryKey, config.url, config.callbacks, config.options);
+    });
+  });
+  async function useQueryCache(queryKey, url, callbacks = {}, options = {}) {
+    const { onSuccess, onError, onFinally } = callbacks;
+    const { ttl = 0, selection = (r) => r.json(), refetchOnFocus = false } = options;
+    if (refetchOnFocus) {
+      _watchedQueries.set(queryKey, { url, callbacks, options });
+    }
     const now = Date.now();
     try {
       if (_internalCache.has(queryKey)) {
-        const { data, status, timestamp } = _internalCache.get(queryKey);
-        if (now - timestamp <= ttl) {
-          onSuccess?.(data, status);
-          return response;
+        const entry = _internalCache.get(queryKey);
+        if (now - entry.timestamp <= ttl) {
+          onSuccess?.(entry.data, entry.status);
+          return entry.data; 
         }
-        _internalCache.delete(queryKey);
       }
       const response = await fetch(url);
-      const isSuccess = response.status >= 200 && response.status < 300;
-
-      if (isSuccess) {
-        let data;
-          data = await selection(response)
-        _internalCache.set(queryKey, { data, status: response.status, timestamp: now });
-        onSuccess?.(data, response.status);
-      } else {
-        const errorContext = { 
-          status: response.status,
-          data: response.data 
-        };
-        throw errorContext;
-      }
-      return response;
+      if (!response.ok) throw { status: response.status };
+      const data = await selection(response);
+      _internalCache.set(queryKey, { data, status: response.status, timestamp: now });
+      onSuccess?.(data, response.status);
+      return data;
     } catch (error) {
-      if (onError) {
-        onError(error.status ? error : { message: error.message, status: 500 });
-      }
+      onError?.(error.status ? error : { message: error.message, status: 500 });
       throw error;
     } finally {
       onFinally?.();
     }
+  }
+
+  return {
+    use: useQueryCache,
+    refetch,
   };
 };
 
