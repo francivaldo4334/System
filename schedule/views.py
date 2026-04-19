385817ac144cff5acf4701301c099cfc10ad7034
francivaldo4334 <francivaldodev@gmail.com>
2026-04-19 12:59:39 -0300

    save

--- schedule/views.py
+++ schedule/views.py
@@ -1,10 +1,20 @@
 # pyright: reportAttributeAccessIssue=false
-from django.db import transaction
+from django.db.models.deletion import ProtectedError
 from rest_framework import viewsets
+from rest_framework.exceptions import APIException
 from rest_framework.generics import ListAPIView
 from schedule.filters import AvailabilityPresentationFilterSet, ResourceFilterSet
 from schedule.models import Assignment, Availability, Resource, Service, ServiceResourceRelation
-from schedule.serializers import AssignmentSerializer, AvailabilityPresentationSerializer, AvailabilitySerializer, CreateAssigmentSerializer, ResourceSerializer, ServiceResourceRelationSerializer, ServiceSerializer
+from schedule.serializers import (
+        AssignmentSerializer,
+        AvailabilityPresentationSerializer,
+        AvailabilitySerializer,
+        CreateAssigmentSerializer,
+        ResourceSerializer,
+        ServiceResourceRelationSerializer,
+        ServiceSerializer
+    )
+from django.utils.translation import gettext_lazy as _
 
 # Create your views here.
 class ResourceViewSet(viewsets.ModelViewSet):
@@ -12,6 +22,13 @@ class ResourceViewSet(viewsets.ModelViewSet):
     serializer_class = ResourceSerializer
     filterset_class = ResourceFilterSet
 
+    def perform_destroy(self, instance):
+        try:
+            instance.delete()
+        except ProtectedError:
+            error = APIException(_("Deletion of %(name)s failed") % {'name': instance._meta.verbose_name})
+            error.status_code = 409
+            raise error
 
 class ServiceViewSet(viewsets.ModelViewSet):
     queryset = Service.objects.all()
