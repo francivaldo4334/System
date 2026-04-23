1c9fa2fc99dfdf7a70a76e2b5568b31fe0f35a94
francivaldo4334 <francivaldodev@gmail.com>
2026-04-23 09:19:46 -0300

    save

--- users/serializers.py
+++ users/serializers.py
@@ -4,8 +4,9 @@ from users.models import ClientBadge
 
 
 class ClientBadgeSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source='user.full_name')
     class Meta:
         model = ClientBadge
         fields = [
-            'id'
+            'full_name'
         ]
