b018c2d3752e71da96f5204a8b076f88b72ca3f5
francivaldo4334 <francivaldodev@gmail.com>
2026-04-26 17:15:59 -0300

    save

--- schedule/flows.py
+++ schedule/flows.py
@@ -1,17 +1,12 @@
 # pyright: reportGeneralTypeIssues=false
-from collections import Counter
 from django.db import transaction
-from django.db.models import QuerySet
-from typing import List, cast
-
-from schedule.utils import AssignmentUtil
+from schedule import utils
 
 
 class AssignmentState:
-    def __init__(self, instance) -> None:
-        from schedule.models import Assignment
-        self.util = AssignmentUtil()
-        self.instance = cast(Assignment, instance)
+    from schedule.models import Assignment
+    def __init__(self, instance:Assignment) -> None:
+        self.instance = instance
 
     def confirm(self):
         raise NotImplementedError(f'Status:{self.instance.get_status_display()}') # type: ignore
@@ -30,41 +25,15 @@ class AssignmentState:
     
 class AssignmentStatePeding(AssignmentState):
     def confirm(self):
-        from schedule.models import ResourceOccupation, Resource, Service, ServiceResourceRelation
-
         with transaction.atomic():
-            # 1. Validação
-            # 1.1 Carrega variaveis para validação
-            service = cast(Service,self.instance.service)
-            service_resource_relation_qs = cast(QuerySet, getattr(ServiceResourceRelation, 'objects'))
-            service_resource_relation_resource_type_id_and_quantity = \
-                service_resource_relation_qs.filter(service=service).values_list('resource_type_id', 'quantity')
-            service_required_resource_quantity_map = dict(service_resource_relation_resource_type_id_and_quantity)
-            instance_resource_qs = cast(QuerySet, self.instance.resources)
-            instance_resource_paret_id_and_id = list(instance_resource_qs.values('parent_id', 'id'))
-            instance_resource_ids = [it['id'] for it in instance_resource_paret_id_and_id]
-            resource_occupation_qs = cast(ResourceOccupation.QuerySet, ResourceOccupation.objects)
-            instance_resource_parent_id_quantity_map = Counter([it['parent_id'] for it in instance_resource_paret_id_and_id])
-            # 1.2 validar resources requiridos de acordo com o tipo de resource do serviço
-            for id, qty in service_required_resource_quantity_map.items():
-                if id not in instance_resource_parent_id_quantity_map.keys():
-                    raise self.ResourceNotAllowed()
-                if qty != \
-                   instance_resource_parent_id_quantity_map[id]:
-                    raise self.ReourceQuantityNotEguals()
-            # 2. Obter a ocupações dos resource usados
-            for resource_id in instance_resource_ids:
-                occupation, _ = resource_occupation_qs.get_or_create(
-                    resource_id=resource_id,
-                    date=self.instance.date,
-                )
-            # 2.1 Verifica se todas as ocupações usadas tem disponibilidade
-                occ_qs = resource_occupation_qs.filter(pk=getattr(occupation, 'pk')).select_for_update()
-                if not occ_qs.available(self.instance.start_slot,
-                                                       self.instance.duration_slot).exists():
-                    raise self.ResourceOcuppied()
-            # 2.2 Atualiza a ocupação do resource usado
-                occ_qs.occupy(self.instance.start_slot, self.instance.duration_slot)
+            rules = utils.AssignmentUtil(self.instance)
+            try:
+                rules.checkServiceRequirements()
+            except utils.ServiceIsRequired:
+                rules.checkResourceOccupations()
+            except Exception as e:
+                raise e
+            rules.occupyTimeSlot()
             self.instance.status = self.instance.Status.CONFIRMED.value # type: ignore
             self.instance.save()
             
--- schedule/utils.py
+++ schedule/utils.py
@@ -1,4 +1,11 @@
+# pyright: reportAssignmentType=false
+# pyright: reportAttributeAccessIssue=false
+# pyright: reportArgumentType=false
 # Exceptions
+
+from typing import List, cast
+
+
 class ResourceNotAllowed(Exception):
     pass
 
@@ -7,6 +14,60 @@ class ReourceQuantityNotEguals(Exception):
 
 class ResourceOcuppied(Exception):
     pass
+
+class ServiceIsRequired(Exception):
+    pass
 # Utils
 class AssignmentUtil:
-    ...
+    from schedule.models import Assignment
+
+    def __init__(self, assigment: Assignment) -> None:
+        self.assignment = assigment;
+
+    def checkServiceRequirements(self):
+        from schedule.models import Service, ServiceResourceRelation, ResourceSelectable
+
+        if isinstance(self.assignment.service, Service):
+            raise ServiceIsRequired();
+
+        service: Service = self.assignment.service
+        service_requirements: List[ServiceResourceRelation] = list(ServiceResourceRelation.objects.filter( service=service ))
+        resources:List[ResourceSelectable] = list(self.assignment.resources.all())
+
+        for service_requirement in service_requirements:
+            resource_count = len([it for it in resources if it.parent_id == service_requirement.resource_type_id])
+            if resource_count != service_requirement.quantity:
+                raise ReourceQuantityNotEguals()
+
+        total_required_resources = sum([it.quantity for it in service_requirements]);
+        total_resources = len(resources)
+
+        if total_required_resources  != total_resources:
+            raise ResourceNotAllowed()
+        
+    def checkResourceOccupations(self):
+        from schedule.models import ResourceSelectable, ResourceOccupation
+
+        resources:List[ResourceSelectable] = list(self.assignment.resources)
+
+        for resource in resources:
+            occupation, _ = ResourceOccupation.objects.get_or_create(
+                resource=resource,
+                date=self.assignment.date,
+            )
+            occupation_queryset:ResourceOccupation.QuerySet = ResourceOccupation.objects.filter(pk=occupation.pk)
+            occupation_queryset.select_for_update()
+            is_available = occupation_queryset.available(
+                start_slot=self.assignment.start_slot,
+                duration_slot=self.assignment.duration_slot
+            ).exists()
+            if not is_available:
+                raise ResourceOcuppied()
+
+    def occupyTimeSlot(self):
+        from schedule.models import ResourceOccupation
+        occupation_queryset:ResourceOccupation.QuerySet = ResourceOccupation.objects.all()
+        occupation_queryset.occupy(
+            start_slot=self.assignment.start_slot,
+            duration_slot=self.assignment.duration_slot,
+        )
