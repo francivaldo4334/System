8b1a50d89c2a82bf91c3455fab492f3f519db181
francivaldo4334 <francivaldodev@gmail.com>
2026-05-07 16:26:55 -0300

    asve

--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+from core.models import ActivatorModel, TimeStampedModel
+
 # Create your models here.
+class ScheduleAppConfig(TimeStampedModel, ActivatorModel):
+    company_image = models.ImageField(
+        null=True,
+        black=True,
+    )
+    company_name = models.CharField(
+        null=True,
+        black=True,
+    )
+    background_image = models.ImageField(
+        null=True,
+        black=True,
+    )
+    resource_visible_to_self_scheduling = models.ManyToManyField(
+        'uri.URIModel',
+    )
+
+    class ActiveScheduleAppConfigExists(Exception):
+        pass
+
+    def save(self, *, force_insert=False, force_update=False, using=None, update_fields=None):
+        if self.__class__.objects.filter(status=self.StatusChoice.ACTIVE.value).exists():
+            raise self.ActiveScheduleAppConfigExists()
+        return super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
--- core/models.py
+++ core/models.py
@@ -46,28 +46,3 @@ class DescriptionModel(models.Model):
 class TitleDescriptionModel(TitleModel, DescriptionModel):
     class Meta:
         abstract = True
-# Common Models
-class ScheduleAppConfig(TimeStampedModel, ActivatorModel):
-    company_image = models.ImageField(
-        null=True,
-        black=True,
-    )
-    company_name = models.CharField(
-        null=True,
-        black=True,
-    )
-    background_image = models.ImageField(
-        null=True,
-        black=True,
-    )
-    resource_visible_to_self_scheduling = models.ManyToManyField(
-        'uri.URIModel',
-    )
-
-    class ActiveScheduleAppConfigExists(Exception):
-        pass
-
-    def save(self, *, force_insert=False, force_update=False, using=None, update_fields=None):
-        if self.__class__.objects.filter(status=self.StatusChoice.ACTIVE.value).exists():
-            raise self.ActiveScheduleAppConfigExists()
-        return super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
