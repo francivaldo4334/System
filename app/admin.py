e6d7d2d9d91741dbeefab93fc14d6af2d3beb631
francivaldo4334 <francivaldodev@gmail.com>
2026-05-08 08:54:07 -0300

    save

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from app.models import AppConfig, ResourceSlugVisible
+
 # Register your models here.
+@admin.register(AppConfig)
+class AppConfigAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ResourceSlugVisible)
+class ResourceSlugVisibleAdmin(admin.ModelAdmin):
+    pass
