48fd63837336d70b997f3b184a750f40f52cb6d6
francivaldo4334 <francivaldodev@gmail.com>
2026-05-20 15:59:37 -0300

    save

--- app/forms.py
+++ app/forms.py
@@ -219,7 +219,9 @@ class ServiceRequirementsForm(BaseForm):
     ]
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = UserCreationForm.Meta.fields + ('email',)
+        fields = UserCreationForm.Meta.fields + ('email','first_name', 'last_name')
