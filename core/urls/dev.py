7a99242f2833fbb3b5c351a816d5475a558af797
francivaldo4334 <francivaldodev@gmail.com>
2026-05-30 16:33:59 -0300

    save

--- core/urls/dev.py
+++ core/urls/dev.py
@@ -1,21 +1,34 @@
-from .api import urlpatterns as api_urls
-from .ssr import urlpatterns as ssr_urls
-from django.urls import path, include
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularSwaggerView,
-    SpectacularRedocView,
-)
-
-urlpatterns = api_urls  + [
-    path("__reload__/", include("django_browser_reload.urls")),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-]
-urlpatterns += ssr_urls
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
+from .api import urlpatterns as api_urls
+from .ssr import urlpatterns as ssr_urls
+
+# 1. Rotas base (API e SSR)
+urlpatterns = api_urls + ssr_urls
+
+# 2. Rotas exclusivas de Desenvolvimento / Debug
 if settings.DEBUG:
+    docs_urls = [
+        path("__reload__/", include("django_browser_reload.urls")),
+        path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+        path(
+            "api/schema/swagger-ui/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path(
+            "api/schema/redoc/",
+            SpectacularRedocView.as_view(url_name="schema"),
+            name="redoc",
+        ),
+    ]
+    
+    urlpatterns += docs_urls
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
