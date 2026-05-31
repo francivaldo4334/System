86dbba633e4be9497876cc5beb70d2d5060fcdfa
francivaldo4334 <francivaldodev@gmail.com>
2026-05-31 16:09:08 -0300

    save

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from authentication.views import EmailViewSet, TriggerClientRemindersView, waiting_email_confirmation
+from authentication.views import EmailViewSet, TriggerClientRemindersAPIView, waiting_email_confirmation
 
 router = routers.SimpleRouter()
 router.register('email', EmailViewSet, 'send_email')
@@ -9,6 +9,6 @@ router.register('email', EmailViewSet, 'send_email')
 
 urlpatterns = [
     path('waiting_confirmation', waiting_email_confirmation, name="waiting_email_confirmation"),
-    path('api/cron/send-reminders/', TriggerClientRemindersView.as_view(), name='cron-client-reminders'),
+    path('api/cron/send-reminders/', TriggerClientRemindersAPIView.as_view(), name='cron-client-reminders'),
     path('', include(router.urls))
 ]
