31b14b87c4dacf84a0ec0e85d54aa91683637877
francivaldo4334 <francivaldodev@gmail.com>
2026-05-31 20:53:46 -0300

    save

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -15,18 +15,18 @@ ALLOWED_HOSTS = ['*']
 ROOT_URLCONF = 'core.urls.dev'
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': config('DB_NAME'),
-    #     'USER': config('DB_USER'),
-    #     'PASSWORD': config('DB_PASSWORD'),
-    #     'HOST': config('DB_HOST', default='127.0.0.1'),
-    #     'PORT': config('DB_PORT', default='5432', cast=int),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='127.0.0.1'),
+        'PORT': config('DB_PORT', default='5432', cast=int),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 REST_FRAMEWORK['DEFAULT_SCHEMA_CLASS'] = 'drf_spectacular.openapi.AutoSchema'
 MIDDLEWARE += [
