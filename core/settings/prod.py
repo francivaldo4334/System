8fbf0c16e2359469eef20c0e22043a115b3443af
francivaldo4334 <francivaldodev@gmail.com>
2026-05-31 18:27:43 -0300

    sve

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,7 +1,43 @@
-# Em produção (Exemplo com SMTP)
+# DEBUG = False
+# ALLOWED_HOSTS = ['meudominio.com.br']
+# ROOT_URLCONF = 'core.urls.dev'
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# # Em produção (Exemplo com SMTP)
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 # EMAIL_HOST_USER = 'seu-email@gmail.com'
 # EMAIL_HOST_PASSWORD = 'sua-senha-de-aplicativo'
+from decouple import config, Csv
+from pathlib import Path
+
+# Configurações Principais
+DEBUG = False
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ROOT_URLCONF = config('ROOT_URLCONF', default='core.urls.dev')
+
+# Banco de Dados
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='127.0.0.1'),
+        'PORT': config('DB_PORT', default='5432', cast=int),
+    }
+}
+
+# Configurações de E-mail (SMTP)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
