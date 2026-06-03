83cc50dfd6d6f7ca89855890f022cef1fc9fa5d8
francivaldo4334 <francivaldodev@gmail.com>
2026-06-03 09:09:33 -0300

    save

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
 
 from authentication.models import CustomUser
 from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {"fields": ("username", "password", 'uid')}),
