a588e8095999900f850f4fd7742b33a16977c158
francivaldo4334 <francivaldodev@gmail.com>
2026-06-04 17:08:52 -0300

    save

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -29,3 +29,8 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
