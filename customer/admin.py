c77587e3c3754937233f864ff02bf59cefd0d661
francivaldo4334 <francivaldodev@gmail.com>
2026-06-05 10:08:53 -0300

    save

--- customer/admin.py
+++ customer/admin.py
@@ -3,8 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from django_tenants.admin import TenantAdminMixin
 
-from customer.models import Client
+from customer.models import Client, Domain
 
 @admin.register(Client)
 class ClientAdmin(TenantAdminMixin, admin.ModelAdmin):
         list_display = ('name',)
+
+@admin.register(Domain)
+class DomainAdmin(admin.ModelAdmin):
+        pass
