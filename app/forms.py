3e3bb39961ccd63100f51b5c20d32be94b9705ac
francivaldo4334 <francivaldodev@gmail.com>
2026-06-06 11:01:27 -0300

    save

--- app/forms.py
+++ app/forms.py
@@ -269,10 +269,13 @@ class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
+    whatsapp_phone = forms.CharField(
+        required=False,
+    )
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('email', 'first_name', 'last_name')
+        fields = UserCreationForm.Meta.fields + ('email', 'first_name', 'last_name', 'whatsapp_phone')
 
     def __init__(self, *args, **kwargs):
         # Remove o request dos kwargs para não quebrar o formulário base
--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
@@ -11,4 +12,14 @@ class CustomUser(AbstractUser):
     )
     is_email_checked = models.BooleanField(
         default=False # type: ignore
+    )
+    whatsapp_phone = models.CharField(
+        max_length=25,
+        null=True,
+        blank=True,
+        validators=[
+            RegexValidator(
+                regex=r'^\+?1?\d{9,15}$',
+            )
+        ]
     )
