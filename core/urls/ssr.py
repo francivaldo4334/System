21092f7ab10aba828a6eed51573df81a0023e269
francivaldo4334 <francivaldodev@gmail.com>
2026-06-06 10:13:06 -0300

    save

--- core/urls/ssr.py
+++ core/urls/ssr.py
@@ -5,6 +5,13 @@ from django.conf.urls.i18n import i18n_patterns
 
 from core.views import LandingPageView
 
+def redirec_to_app(view):
+    def _view(request,*args, **kwargs):
+        if request.tenant.is_master:
+            return view(request,*args, **kwargs)
+        return redirect(reverse('app'))
+    return _view
+
 # def to_app_page(request):
 #     if hasattr(request.user, 'is_email_checked') and request.user.is_email_checked:
 #         return redirect('/app/')
@@ -12,7 +19,7 @@ from core.views import LandingPageView
 
 
 urlpatterns = i18n_patterns(
-    path('',LandingPageView.as_view(), name='home'),
+    path('',redirec_to_app(LandingPageView.as_view()), name='home'),
     path('admin/', admin.site.urls),
     # path('', to_app_page),
     path('app/', include('app.urls')),
