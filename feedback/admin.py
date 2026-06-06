64eaa975e3d33e0691727632f42ce1d968c93ead
francivaldo4334 <francivaldodev@gmail.com>
2026-06-06 20:30:51 -0300

    save

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from feedback.models import Message, Response
+
 # Register your models here.
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
+    pass
--- feedback/serializers.py
+++ feedback/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 
-from feedback.models import Message
+from feedback.models import Message, Response
+
+
+class ResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Response
+        fields = [
+            'text',
+            'is_viewed',
+        ]
 
 
 class MessageSerializer(serializers.ModelSerializer):
+    response_set = ResponseSerializer(many=True)
     class Meta:
         model = Message
         fields = [
@@ -13,12 +23,14 @@ class MessageSerializer(serializers.ModelSerializer):
             'created',
             'modified',
             'created_by',
+            'response_set',
         ]
         read_only_fields = [
             'status',
             'created',
             'modified',
             'created_by',
+            'response_set',
         ]
     def save(self, **kwargs):
         request = self.context.get('request')
