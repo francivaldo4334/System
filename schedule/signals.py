e0ccc49c6f10f75c61991be9887f5ce6614a4fb1
francivaldo4334 <francivaldodev@gmail.com>
2026-06-06 10:07:37 -0300

    save

--- schedule/signals.py
+++ schedule/signals.py
@@ -1,11 +1,13 @@
-from django.contrib.auth.models import Group, User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 @receiver(post_save, sender=User)
 def atribuir_grupo_cliente(sender, instance, created, **kwargs):
-    """Sinal que adiciona automaticamente o usuário recém-criado ao grupo 'client'."""
     if created:
-        grupo_client, _ = Group.objects.get_or_create(name="CLIENT")
-        instance.groups.add(grupo_client)
+        # Certifique-se de que o grupo 'Clientes' já existe no banco
+        grupo_cliente, _ = Group.objects.get_or_create(name="CLIENT")
+        instance.groups.add(grupo_cliente)
