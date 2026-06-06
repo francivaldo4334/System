692be8464f6fa1abc3a90db535f0bcd3822f3956
francivaldo4334 <francivaldodev@gmail.com>
2026-06-06 08:11:37 -0300

    save

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, reverse
 from django.views.generic import TemplateView
 from rest_framework import viewsets
+from rest_framework.response import Response
+from django.utils.translation import gettext_lazy as _
 
 from core.permissions import IsOwner
 from django.apps import apps
@@ -13,7 +15,22 @@ UserModel = apps.get_model(settings.AUTH_USER_MODEL, require_ready=False)
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [IsOwner]
     serializer_class = UserSerializer
-    queryset = UserModel.objects.all().filter(is_superuser=False)
+    queryset = UserModel.objects.all().filter(is_superuser=False, is_active=True)
+
+    class NoExcludeSelfUser(Exception):
+        pass
+
+    def perform_destroy(self, instance):
+        if instance.pk == self.request.user.pk:
+            raise self.NoExcludeSelfUser()
+        instance.is_active = False
+        instance.save()
+
+    def handle_exception(self, exc):
+        try:
+            return super().handle_exception(exc)
+        except self.NoExcludeSelfUser:
+            return Response(_("You cannot delete your own user account."), 405)
 
 class LandingPageView(TemplateView):
     template_name = "pages/landing-page/index.html"
