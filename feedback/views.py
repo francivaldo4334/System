a4607ec4126262e8716d00dcacc483e2e4722961
francivaldo4334 <francivaldodev@gmail.com>
2026-06-06 21:16:57 -0300

    save

--- feedback/views.py
+++ feedback/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
 
-from feedback.models import Message
+from feedback.models import Message, Response
 from feedback.serializers import MessageSerializer
 
 # Create your views here.
@@ -12,6 +13,15 @@ class MessageViewSet(
 ):
     serializer_class = MessageSerializer
     queryset = Message.objects.all()
+
+    @action(["POST"], False)
+    def mark_viewed(self, request):
+        Response.objects.filter(
+            feedback__created_by=self.request.user
+        ).update(
+            is_viewed=True,
+        )
+        return Response()
 
     def get_queryset(self):
         return super().get_queryset().filter(
